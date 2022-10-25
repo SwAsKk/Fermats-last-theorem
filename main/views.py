b7617e1649adabd24767b12c9b93974b1f078c0d
Sweet As KaraKum <76500727+SwAsKk@users.noreply.github.com>
2022-10-25 20:47:54 +0300

    Made Main page
    -Connected jQuery
    -Fixed footer
    -Fixed header, added sandwich menu
    -Fixed context

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+import numpy as np
 # Create your views here.
 def base_context(request):
     context = dict()
     context['user'] = request.user
     return context 
 def index(request):
-    context = {}
+    context = base_context(request)
+    context["site_name"] = "Последняя теорема Фарма"  # Строка перед | в title страницы
+    context["page_name"] = "Главная"  # Строка после |
     return render(request,'pages/index.html', context)
