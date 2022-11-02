09fffaad5aa0c25b0769e5cacba62e6b956873bf
Sweet As KaraKum <lev.sobolev.03@mail.ru>
2022-11-02 21:53:22 +0300

    Trash cleaning and finishing of function
    Cleaned trash and finished function.
    Made comments for code
    TODO:
    -instruction for users (Modal window)
    -Fix styles
    -Styles for answer of function

--- main/views.py
+++ main/views.py
@@ -12,36 +12,44 @@ def base_context(request):
     context = dict()
     context['user'] = request.user
     return context 
+#View for index page
 def index(request):
     context = base_context(request)
     context["site_name"] = "Последняя теорема Фарма"  # Строка перед | в title страницы
     context["page_name"] = "Главная"  # Строка после |
     return render(request,'pages/index.html', context)
 
-
+#View for numeric methods page
 def numericMehods(request):
     context = base_context(request)
     context["site_name"] = "Численные методы"
     context["page_name"] = "Главная"
     return render(request, 'pages/numericMethods.html', context)
 
-
+#View for matrix rules in numeric methods page
 def matrixLawOfEquality(request):  
     context = {}
     context["site_name"] = "Численные методы"
     context["page_name"] = "Законы матриц"
-    #firstArray = np.array(((2,2)), dtype="int32")
+
+    #Get size of array from user input and get it into context
     if request.method == "POST":
         context["n_value"] = range(int(request.POST.get('size', 0)))
         
-    if request.POST.get('size') != 0:
-            sizeOfArray = int(request.POST.get('size', 0))
-    print(sizeOfArray)
-      
+    #Get list of user input (first matrix) return listArray = ['2','3','5' etc.] 
     listArray = request.POST.getlist('fake_matrix')
+
+    #Get list of user input (second matrix) return listArray = ['2','3','5' etc.] 
+    listArray2 = request.POST.getlist('fake_matrix_second')
+
+    #Declaring of first array
     fisrtArray = []
+
+    #Declaring of second array 
+    secondArray = []
+
     
-    
+    #Filling first array using listArray. (round(len(listArray)**0.5)) is for correct filling. Saying easy it's size of arrays 
     if len(listArray) != 0:
         for с in range(round(len(listArray)**0.5)):
             fisrtArray.append([0]*round(len(listArray)**0.5))
@@ -51,12 +59,49 @@ def matrixLawOfEquality(request):
                 fisrtArray[i][j] = int(listArray[count])
                 count += 1
 
-    print(fisrtArray)
+    #Filling second array using listArray2
+    if len(listArray2) !=0:
+        for g in range(round(len(listArray)**0.5)):
+            secondArray.append([0]*round(len(listArray)**0.5))
+        count1 = 0
+        for i in range(round(len(listArray)**0.5)):
+            for j in range(round(len(listArray)**0.5)):
+                secondArray[i][j] = int(listArray2[count1])
+                count1 +=1
+
+    #Declaration of third array
+    thirdArray = np.array
+
+    #Converting first and second array to NumPy array
+    np.asarray(fisrtArray)
+    np.asarray(secondArray)
+
+    #Getting the value of third matrix using multiplication of first and second matrix
+    thirdArray = np.dot(fisrtArray, secondArray)
+
+    #Summ of first and second matrix (A+B)
+    sumOfFirstAndSecondMatrix = np.add(fisrtArray, secondArray)
+
+    #Result of left side of rule (A+B)*C
+    resultOfLeftSide = np.dot(sumOfFirstAndSecondMatrix, thirdArray)
+
+    #Multiplication of first and third matrix (AC)
+    multiplicationOfFirstAndThirdMatrix = np.dot(fisrtArray, thirdArray)
     
-    #for i in range(2):
-        #for j in range(2):
-            #firstArray[i][j].insert(firstArray,listArray)
-    print(listArray) 
+    #Multiplication of second and third matrix (BC)
+    multiplicationOfSecondAndThirdMatrix = np.dot(secondArray, thirdArray)
+
+    #Result of right side of rule = AC + BC
+    resultOfRightSide = np.add(multiplicationOfFirstAndThirdMatrix, multiplicationOfSecondAndThirdMatrix)
+
+
+
+
+
+    #Getting first and second array to context to display on site
     context["firstArray"] = fisrtArray
-    
+    context["secondArray"] = secondArray
+    context["thirdArray"] = thirdArray
+    context["resultOfLeftSide"] = resultOfLeftSide
+    context["resultOfRightSide"] = resultOfRightSide
     return render(request, 'pages/matrixlaws.html', context)
