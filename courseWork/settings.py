49fc9a97b8e72f51168628b790615ec66db4bbe6
Sweet As KaraKum <76500727+SwAsKk@users.noreply.github.com>
2023-03-30 13:42:17 +0300

    Deleted env

--- courseWork/settings.py
+++ courseWork/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-u)0^dr%*x!dlkg+x112ihh!1hwy$j#80osb=8e3bz9eg(wyetf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
